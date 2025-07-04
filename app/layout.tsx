82e2d6d7ab41af904be7278db463def23f5c75ff
Ebubeker <rexhaebubeker@gmail.com>
2025-07-04 12:04:38 +0200

    fix: meta data changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,28 @@ import Footer from "./_components/Footer";
 export const metadata: Metadata = {
   title: "BekerDev | Portfolio",
   description:
-    "A 20 year old Web Developer from Albania with 2.5 years of professional experience. I have worked using React JS, Next JS, Vue JS, Node JS and python",
+    "A 20-year-old Web Developer from Albania with 2.5 years of professional experience, skilled in building modern web applications using React.js, Next.js, Vue.js, Node.js, and Python.",
+  openGraph: {
+    title: "BekerDev | Portfolio",
+    description: "A 20-year-old Web Developer from Albania with 2.5 years of professional experience, skilled in building modern web applications using React.js, Next.js, Vue.js, Node.js, and Python.",
+    url: "https://example.com",
+    siteName: "BekerDev",
+    images: [
+      {
+        url: "https://beker.dev/assets/home.png",
+        width: 1200,
+        height: 630,
+        alt: "Og Image Alt Text",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "BekerDev | Portfolio",
+    description: "A 20-year-old Web Developer from Albania with 2.5 years of professional experience, skilled in building modern web applications using React.js, Next.js, Vue.js, Node.js, and Python.",
+    images: ["https://beker.dev/assets/home.png"],
+  },
 };
 
 export default function RootLayout({
